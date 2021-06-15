9fb6ed059370087bc3cc3a691d69e30f49452d97
cansoup <0311minha@gmail.com>
2021-06-16 08:26:32 +0900

    0616: vue.js 시작하기 마무리

---
+++ vue-form/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+// 이벤트 버블링, 이벤트 캡처, 이벤트 위임
+// https://joshua1988.github.io/web-development/javascript/event-propagation-delegation/
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      // form의 기본 동작을 막아줌
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password,
+      }
+      axios.post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error){
+          console.log(error);
+        });
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
